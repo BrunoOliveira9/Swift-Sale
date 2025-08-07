4c189e819cf18f0f1c7c745e8d706c0f215bf092
Eduardo Abreu <eduardopradoabreu@gmail.com>
2025-08-06 21:09:13 -0300

    implementado autenticação na página de login e ajustes na estrutura do front, criação do .env.example

---
+++ swift-frontend/src/pages/login/Login.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
+import Logo from "../../pages/inicio/SW.svg";
+import { login } from "../../services/auth/authentication-service.ts";
+
+function Login() {
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function signIn() {
+    try {
+      const response = await login(username, password);
+      console.log("Login realizado:", response);
+      navigate("/inicio");
+    } catch (error) {
+      console.error("Erro ao fazer login:", error.message);
+      alert("Usuário ou senha inválidos");
+    }
+  }
+
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <img src={Logo} alt="Logo" className="login-logo" />
+
+        <form onSubmit={(e) => e.preventDefault()}>
+          <h2 className="login-subtitle">Sistema de PDV</h2>
+
+          <p className="login-instruction">Informe usuário e senha para entrar no sistema.</p>
+
+          <input
+            type="text"
+            placeholder="Usuário"
+            className="login-input"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+
+          <input
+            type="password"
+            placeholder="Senha"
+            className="login-input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button className="login-button" type="button" onClick={signIn}>
+            Login
+          </button>
+        </form>
+
+        <div className="login-remember">
+          <input type="checkbox" id="remember" />
+          <label htmlFor="remember">Permanecer conectado</label>
+        </div>
+
+        <footer className="login-footer">© Swift Sistemas</footer>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ swift-frontend/src/services/authentication-service.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_URL = `http://${process.env.REACT_APP_HOST_BACKEND}:${process.env.REACT_APP_PORT_BACKEND}/auth`;
+
+export const login = async (username: string, password: string) => {
+  try {
+    const response = await axios.post(`${API_URL}/login`, { username, password });
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao tentar fazer login", error);
+    throw new Error('Erro ao realizar login');
+  }
+};
