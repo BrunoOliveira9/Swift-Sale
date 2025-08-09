acc30fd6af514e23563499cc494098227a2632ce
BrunoOliveira9 <brunoolv25@gmail.com>
2025-08-09 17:22:37 -0300

    Adição de nova tela de login e página para cadastro de produtos, menu lateral alterado.

--- swift-frontend/src/pages/inicio/Inicio.jsx
+++ swift-frontend/src/pages/inicio/Inicio.jsx
@@ -4,6 +4,24 @@ import logo from './SW.svg';
 import { useAuth } from '../../services/auth/auth-context.service.tsx';
 import showToast from '../../components/toast/Toast.jsx';
 
+import { Link } from 'react-router-dom'; 
+
+// Icones
+import { FaChartPie, FaFileInvoiceDollar, FaChartLine, FaBoxOpen, FaUserFriends, FaCreditCard, FaTools, FaShippingFast, FaFileAlt} from 'react-icons/fa';
+
+// novos icones
+const menuItems = [
+  { title: 'Vendas', icon: <FaChartPie />, color: '#42a5f5', path: '/vendas' },
+  { title: 'Faturamento', icon: <FaFileInvoiceDollar />, color: '#42a5f5', path: '/faturamento' },
+  { title: 'Usuário e Segurança', icon: <FaChartLine />, color: '#42a5f5', path: '/usuarios' },
+  { title: 'Produtos', icon: <FaBoxOpen />, color: '#6d4c41', path: '/produtos' },
+  { title: 'Clientes', icon: <FaUserFriends />, color: '#78909c', path: '/clientes' },
+  { title: 'Financeiro', icon: <FaCreditCard />, color: '#26a69a', path: '/financeiro' },
+  { title: 'Serviços', icon: <FaTools />, color: '#ab47bc', path: '/servicos' },
+  { title: 'Logística', icon: <FaShippingFast />, color: '#00bcd4', path: '/logistica' },
+  { title: 'Relatórios', icon: <FaFileAlt />, color: '#ffa726', path: '/relatorios' },
+];
+
 function Inicio() {
   const [menuAberto, setMenuAberto] = useState(false);
   const { logout } = useAuth();
@@ -25,7 +43,7 @@ function Inicio() {
         </button>
       )}
 
-      {/* Menu lateral */}
+      
       <aside className={`sidebar ${menuAberto ? 'aberto' : ''}`}>
         <button className="close-menu-button" onClick={toggleMenu}>
           ×
@@ -37,12 +55,18 @@ function Inicio() {
           <ul>
             <li><button>Cadastro</button></li>
             <li><button>Registro</button></li>
+            <li><button>Vendas</button></li>
+            <li><button>Faturamento</button></li>
+            <li><button>Usuário e segurança</button></li>
+            <li><button>Produtos</button></li>
+            <li><button>Clientes</button></li>
+            <li><button>Financeiro</button></li>
           </ul>
         </div>
         <div className="menu-section">
           <h3>Configuração</h3>
           <ul>
-            <li><button>Nota Fiscal</button></li>
+            <li><button>Relatórios</button></li>
           </ul>
         </div>
 
@@ -58,13 +82,18 @@ function Inicio() {
           <img src={logo} alt="Swift Logo" className="logo-centered" />
         </div>
 
-        <div className="buttons-section">
-          <div className="buttons-grid">
-            <button className="grid-button">Cadastro</button>
-            <button className="grid-button">Registro</button>
-            <button className="grid-button">Nota Fiscal</button>
-          </div>
-          <p className="instruction">Escolha uma opção</p>
+        
+        <div className="modules-grid">
+          {menuItems.map((item, index) => (
+            <Link to={item.path} key={index} className="module-link">
+              <div className="module-card">
+                <div className="icon-container" style={{ color: item.color }}>
+                  {React.cloneElement(item.icon, { size: 40 })}
+                </div>
+                <p>{item.title}</p>
+              </div>
+            </Link>
+           ))}
         </div>
       </main>
     </div>
